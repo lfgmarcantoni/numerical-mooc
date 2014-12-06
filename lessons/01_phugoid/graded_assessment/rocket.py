87780c8f621f554b24e182f5ffa4b77e0e933f27
lfgmarcantoni <lfgmarcantoni@gmail.com>
2014-12-05 21:23:21 -0300

    NameChange

---
+++ lessons/01_phugoid/graded_assessment/rocket.py
@@ -0,0 +1,71 @@
+import numpy as np 
+
+dt=0.01 # time step 
+t=100.0 # time of computation
+nts=t/dt # number of time steps 
+nts=np.int(nts) 
+ms= 50.0  #Rocket shell weight
+g = 9.81 #Gravitational acceleration
+rho = 1.091 #Air density
+r = 0.5  #Rocket radius 
+A=np.pi*r*r  #Rocket area
+ve = 325.0 # exhaust gas velocity
+CD=0.15 #Drag coeficient
+mp0= 100  #propelant mass at t=0.0
+mpdot = 20.0 #propelant burn rate
+
+mpdt = mp0 - mpdot*t 
+if (t<0.0)|(t>5.0):
+   mpdt=0.0
+msplump = ms+mpdt 
+print 'mpdt=',mpdt
+print 'ms+mp=',msplump
+print 'nts',nts
+#v(0)=1
+print 't',t
+print 'dt',dt
+#times=np.linspace(0, t, float(nts))
+times=np.arange(0.0,(t+dt),dt)
+h=np.zeros(times.size)
+v=np.zeros(times.size)
+h[0]=0.0
+v[0]=0.0
+print'times[1]',times[1]
+print 'times',times  
+print 'dim.times',times.size
+
+F1 = (mpdot/msplump)*ve
+F2 = ((1/2)*rho*A*CD)/msplump
+
+for i in range(times.size-1):
+	mp = mp0 - mpdot*times[i]
+	if (times[i]<0.0)|(times[i]>5.0):
+   	   mp=0.0
+	   mpdot=0.0
+#	h[i+1]=h[i]+v[i]*dt 
+        v[i+1]=(-g+(mpdot/(ms+mp))*ve-(1.0/2.0)*rho*A*CD*v[i]*np.abs(v[i])*(1/(mp+ms)))*dt+v[i]
+        h[i+1]=h[i]+v[i]*dt  
+#        print(times[i],h[i],v[i],mp)
+
+import time as tm
+print 'h max',np.max(h)
+print 'v max',np.max(v)
+print 'h size',h.size
+for i in range(0,h.size):
+#	print 'i',i, 'times',times[i],'vel',v[i]
+ #       tm.sleep(1.0)  
+        if (v[i]<0.0):
+	      print 'max alt time',times[i-1]
+              print 'max alt',h[i-1]
+              break
+for i in range(0,h.size):
+        if(times[i]>5.0)&(h[i]<0.03):
+        	print 'imp time',times[i]
+                print 'imp velocity',v[i] 
+	        break
+
+
+
+
+
+
